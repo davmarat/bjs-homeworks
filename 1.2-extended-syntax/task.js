ff469a61017beb054268c2a45ed9dccf182a54ab
Marat Davletgareev <davletgareev.mn@gmail.com>
2021-02-01 16:12:43 +0300

    first commit

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,45 +1,33 @@
-function getResult(a,b,c){
-    "use strict";
+"use strict"
+function getResult(a,b,c) {
     // код для задачи №1 писать здесь
     // return x;
-    let result ='';
+    let result =[];
     let discrim = b**2 - 4 * a * c; 
-    if (discrim < 0) { 
-      result = [];
-      console.log(result);
-   } else if (discrim === 0) {
+    if (discrim === 0) {
        result = [(-b + Math.sqrt(discrim)) / (2*a)];
-       console.log(result);
    } else if (discrim > 0) {
        result = [(-b + Math.sqrt(discrim)) / (2*a), (-b - Math.sqrt(discrim)) / (2*a)];
-       console.log(result);
    }
-  
      return result;
-}
+};
 
 
 function getAverageMark(marks){
     // код для задачи №2 писать здесь
     // return averageMark;
     let sumResult = 0;
-    let averageMark = '';
+    let averageMark;
     const numberMarks = 5;
     for (let i = 0; i < marks.length; i++) {
-      sumResult = sumResult + marks[i];
-      if (marks.length > numberMarks) {
-          console.log(marks.length);   
-          marks.splice(numberMarks);
-      } else if (averageMark = 0) {
-          console.log("0")
-      }
-    }
-
-    averageMark = sumResult / marks.length;
-    console.log(averageMark);
-    
-    return averageMark;
-}
+       sumResult = sumResult + marks[i];
+       console.log(marks.length);   
+       marks.splice(numberMarks);
+    };
+    if (sumResult === 0){
+         return sumResult;
+    };
+        return sumResult / marks.length;
+};
 
 
-}
