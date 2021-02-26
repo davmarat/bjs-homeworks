1f615f8c772a89f8e0932acf7ed475301fbe8df2
Marat Davletgareev <davletgareev.mn@gmail.com>
2021-02-26 16:50:43 +0300

    first commit

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,31 @@
 //String.prototype.isPalindrome - для задачи №1
+"use strict"
+
+String.prototype.isPalindrome = function() {
+    const normalString = this.toLowerCase().replaceAll(" ", "");
+    const reversString = normalString.split('').reverse().join('').replaceAll(" ", "");
+    console.log(reversString);
+    console.log(normalString);
+    return reversString === normalString;
+}
+
+
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
-}
+    let sum = 0;
+    let marksAverage;
+    let roundedAverage;
+        
+    if (marks.length === 0) {
+           return 0;
+        }
+        
+    for (let i = 0; i < marks.length; i++) {
+               sum += marks[i];
+           }
+    marksAverage = sum / marks.length;
+    roundedAverage = Math.round(marksAverage);
+    return roundedAverage;
+    }
 
-function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
-}
+
